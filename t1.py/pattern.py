f0bcaefe8e227a707c1006dbad4ebecff6eb270e
prashu <prashutoshmaurya@gmail.com>
2023-05-30 16:59:02 +0530

    prashu 30 may

---
+++ t1.py/pattern.py
@@ -0,0 +1,11 @@
+from turtle import*
+speed('fastest')
+for i in range(6):
+    fd(100)
+    lt(360/6)
+    for i in range (6):
+        fd(50)
+        lt(360/6)
+        mainloop()
+    
+
--- t1.py/ti.py
+++ t1.py/ti.py
@@ -1,8 +1,2 @@
 from turtle import*#import all functions from turtle module
-fd(100)
-lt(90)
-fd(100)
-lt(90)
-fd(100)
-lt(90)
-mainloop()
+lt(60)
