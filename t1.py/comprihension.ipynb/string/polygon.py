0d042136b1f501e2f3c1cead659ec9fe3b137775
prashu <prashutoshmaurya@gmail.com>
2023-06-21 16:59:10 +0530

    functions

---
+++ t1.py/comprihension.ipynb/string/polygon.py
@@ -0,0 +1,19 @@
+from turtle import*
+def triangle()
+    for i in range(3):
+        fd(100)
+        lt(60)
+def square()
+    for i in range(4):
+        fd(100)
+        lt(90)
+
+def hexagon(d):
+    for i in range(6):
+        fd(d)
+        lt(60)
+def pentagon():
+    for i in range(5):
+        fd(100)
+        lt(72)
+   
---
+++ t1.py/comprihension.ipynb/string/t10.py
@@ -0,0 +1,20 @@
+from turtle import*
+from polygon import*
+speed('fastest')
+for i in range(5):
+    hexagon()
+    for i in range(6):
+        hexagon(100)
+        for i in range(6):
+            hexagon(50)
+            circle(50)
+            lt(65)
+        lt(50)
+hideturtle() 
+mainloop()           
+        
+        
+        
+    
+
+
